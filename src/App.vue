e9a1195a3d3065f00c0cc1ff1d0191ab65676d59
Aurélien <rofldu08@gmail.com>
2023-10-04 16:30:20 +0200

    vue de l'accueil

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <div id="app">
     <RandomStudent />
+    <RandomStudentAbsent />
   </div>
 </template>
 
 <script>
 import RandomStudent from "./components/RandomStudent.vue";
+import RandomStudentAbsent from "@/components/RandomStudentAbsent.vue";
 
 export default {
   components: {
+    RandomStudentAbsent,
     RandomStudent
   }
 };
