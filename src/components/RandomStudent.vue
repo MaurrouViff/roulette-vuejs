ff2c98012b69d9b2a6436834a82864f2593e72b9
Aurélien <rofldu08@gmail.com>
2023-10-04 14:56:24 +0200

    composant qui gère le tirage au sort

--- src/components/RandomStudent.vue
+++ src/components/RandomStudent.vue
@@ -1,7 +1,14 @@
 <template>
   <div>
     <h1>Tirage au sort d'un élève</h1>
-    <button @click="tirerAuSort">Tirer au sort</button>
+    <div>
+      <label for="classe">Sélectionnez une classe :</label>
+      <select v-model="classeSelectionnee" id="classe">
+        <option value="">Toutes les classes</option>
+        <option v-for="classe in classes" :key="classe">{{ classe }}</option>
+      </select>
+      <button @click="tirerAuSort">Tirer au sort</button>
+    </div>
     <div v-if="eleveTire">
       <p>ID: {{ eleveTire.id }}</p>
       <p>Nom: {{ eleveTire.nomfamille }}</p>
@@ -19,14 +26,23 @@ export default {
   data() {
     return {
       eleves: [],
-      eleveTire: null
+      eleveTire: null,
+      classes: [], // Tableau des classes disponibles
+      classeSelectionnee: '' // Classe sélectionnée dans le menu dépliant
     };
   },
   methods: {
     tirerAuSort() {
-      if (this.eleves.length > 0) {
-        const randomIndex = Math.floor(Math.random() * this.eleves.length);
-        this.eleveTire = this.eleves[randomIndex];
+      let elevesFiltres = this.eleves;
+
+      if (this.classeSelectionnee !== '') {
+        // Filtrer les élèves par classe si une classe est sélectionnée
+        elevesFiltres = this.eleves.filter(eleve => eleve.classe === this.classeSelectionnee);
+      }
+
+      if (elevesFiltres.length > 0) {
+        const randomIndex = Math.floor(Math.random() * elevesFiltres.length);
+        this.eleveTire = elevesFiltres[randomIndex];
       }
     }
   },
@@ -34,6 +50,9 @@ export default {
     axios.get('/database.json')
         .then(response => {
           this.eleves = response.data.tables[0].data;
+
+          // Extraire les classes uniques du JSON
+          this.classes = [...new Set(this.eleves.map(eleve => eleve.classe))];
         })
         .catch(error => {
           console.error('Erreur lors du chargement des données :', error);
