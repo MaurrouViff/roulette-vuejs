55e5ae6f146f34910a159511f2d67ec38add7994
Aurélien <rofldu08@gmail.com>
2023-10-04 16:30:37 +0200

    liste des élves

---
+++ src/ListeEleve.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <ShowStudents />
+  </div>
+</template>
+
+<script>
+import ShowStudents from "./components/ShowStudents.vue";
+
+
+export default {
+  components: {
+    ShowStudents
+  }
+};
+</script>
